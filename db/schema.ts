e0cf1f7dac8975aa46abea737b3b89f1138e952d
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-24 17:52:54 +0530

    feat: add schema of db & migrations

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from 'drizzle-orm'
 import {
+  boolean,
   index,
   integer,
   pgEnum,
@@ -11,12 +12,13 @@ import {
 } from 'drizzle-orm/pg-core'
 
 export const user = pgTable('user', {
-  id: text().primaryKey(),
-  name: text().notNull(),
-  email: text().notNull().unique(),
-  image: text(),
-  createdAt: timestamp().defaultNow().notNull(),
-  updatedAt: timestamp()
+  id: text('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  emailVerified: boolean('email_verified').default(false).notNull(),
+  image: text('image'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
     .defaultNow()
     .$onUpdate(() => /* @__PURE__ */ new Date())
     .notNull(),
@@ -25,16 +27,16 @@ export const user = pgTable('user', {
 export const session = pgTable(
   'session',
   {
-    id: text().primaryKey(),
-    expiresAt: timestamp().notNull(),
-    token: text().notNull().unique(),
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp()
+    id: text('id').primaryKey(),
+    expiresAt: timestamp('expires_at').notNull(),
+    token: text('token').notNull().unique(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at')
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
-    ipAddress: text(),
-    userAgent: text(),
-    userId: text()
+    ipAddress: text('ip_address'),
+    userAgent: text('user_agent'),
+    userId: text('user_id')
       .notNull()
       .references(() => user.id, { onDelete: 'cascade' }),
   },
@@ -44,21 +46,21 @@ export const session = pgTable(
 export const account = pgTable(
   'account',
   {
-    id: text().primaryKey(),
-    accountId: text().notNull(),
-    providerId: text().notNull(),
-    userId: text()
+    id: text('id').primaryKey(),
+    accountId: text('account_id').notNull(),
+    providerId: text('provider_id').notNull(),
+    userId: text('user_id')
       .notNull()
       .references(() => user.id, { onDelete: 'cascade' }),
-    accessToken: text(),
-    refreshToken: text(),
-    idToken: text(),
-    accessTokenExpiresAt: timestamp(),
-    refreshTokenExpiresAt: timestamp(),
-    scope: text(),
-    password: text(),
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp()
+    accessToken: text('access_token'),
+    refreshToken: text('refresh_token'),
+    idToken: text('id_token'),
+    accessTokenExpiresAt: timestamp('access_token_expires_at'),
+    refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+    scope: text('scope'),
+    password: text('password'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at')
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
   },
