b394ed00a5ad1404dea8d8de253ff7bce92aaa04
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-26 19:16:05 +0530

    feat: add Toaster for ux

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Script from 'next/script'
 import { ThemeProvider } from 'next-themes'
+import { Toaster } from 'sonner'
 import { geistMono, geistSans } from '@/config/font'
 import { siteConfig } from '@/config/site'
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange>
           {children}
+          <Toaster richColors position="top-right" />
         </ThemeProvider>
       </body>
     </html>
